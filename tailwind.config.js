871fe3cb971d4eaf6ff29855492c23c1b92c10cf
AliceLong <al6695@nyu.edu>
2023-09-09 01:30:08 -0400

    basic set up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
-module.exports = {
-	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [
+	  "./index.html",
+	  "./src/**/*.{js,ts,jsx,tsx}",
+	],
 	theme: {
-		extend: {},
+	  extend: {},
 	},
-	variants: {
-		extend: {},
-	},
-	plugins: [],
-};
+	plugins: [
+		require('@tailwindcss/aspect-ratio'),
+	],
+  }
