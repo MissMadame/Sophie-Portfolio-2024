2343f85aba471f8dc61b1025d624151e06adf962
AliceLong <al6695@nyu.edu>
2024-04-18 15:41:13 -0400

    filter update

--- src/Components/ProjectGrids.jsx
+++ src/Components/ProjectGrids.jsx
@@ -1,11 +1,12 @@
-import React from "react";
-import { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import sanityClient from "./client";
 import { Link } from "react-router-dom";
 import "./ProjectGrids.css";
+import Loading from "./Loading"; // Import your loading component
 
 const ProjectGrids = ({ selectedLabels }) => {
   const [projects, setProjects] = useState([]);
+  const [isLoading, setIsLoading] = useState(true); // Add this line
 
   useEffect(() => {
     const fetchProjects = async () => {
@@ -23,41 +24,48 @@ const ProjectGrids = ({ selectedLabels }) => {
     fetchProjects();
   }, []);
 
-  return (
-    <div className="px-[10vw] pt-[5vh] text-sm text-black flex flex-wrap justify-left gap-x-[10vw] gap-y-[5vh] ">
-      {projects
-        .filter(
-          (project) =>
-            selectedLabels.length === 0 ||
-            project.labels.some((label) => selectedLabels.includes(label))
-        )
-        .map((project, index) => (
-          <div key={index} className="bg-transparent overflow-hidden w-72 ">
-            <Link to={`/project/${project.slug.current}`}>
-              <div className="image-container">
-                {" "}
-                {/* Use the new CSS class */}
-                <img
-                  src={project.mainImageUrl}
-                  alt={project.title}
-                  className="hover:cursor-customHover"
-                />
-              </div>
-              <h3 className="pt-2 pl-3 font-BugrinoBold block hover:cursor-customHover">
-                {project.title}
-              </h3>
-            </Link>
+  const handleImageLoad = () => {
+    setIsLoading(false); // Set loading to false when image is loaded
+  };
 
-            <div className="text-sm flex flex-wrap pl-3 ">
-              {project.labels.map((label, labelIndex) => (
-                <span key={labelIndex} className="mr-2">
-                  {label}
-                </span>
-              ))}
+  return (
+    <>
+      {isLoading && <Loading />}{" "}
+      {/* Show loading component when isLoading is true */}
+      <div className="px-[12vw] pt-[5vh] text-sm text-black flex flex-wrap justify-center gap-x-[8vw] gap-y-[5vh] ">
+        {projects
+          .filter(
+            (project) =>
+              selectedLabels.length === 0 ||
+              selectedLabels.every((label) => project.labels.includes(label))
+          )
+          .map((project, index) => (
+            <div key={index} className="bg-transparent overflow-hidden w-72 ">
+              <Link to={`/project/${project.slug.current}`}>
+                <div className="image-container outline-block">
+                  <img
+                    src={project.mainImageUrl}
+                    alt={project.title}
+                    className="hover:border-4 hover:border-black hover:cursor-customHover"
+                    onLoad={handleImageLoad} // Add this line
+                  />
+                </div>
+                <h3 className="pt-2 pl-3 font-BugrinoBold block hover:cursor-customHover">
+                  {project.title}
+                </h3>
+              </Link>
+
+              <div className="text-sm flex flex-wrap pl-3 ">
+                {project.labels.map((label, labelIndex) => (
+                  <span key={labelIndex} className="mr-2">
+                    {label}
+                  </span>
+                ))}
+              </div>
             </div>
-          </div>
-        ))}
-    </div>
+          ))}
+      </div>
+    </>
   );
 };
 
