cbccd40d2affa1120a5f41987da1efd039cada7e
AliceLong <al6695@nyu.edu>
2024-04-18 23:44:49 -0400

    loading fixed

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -4,9 +4,8 @@ function Loading() {
   return (
     <div className="bg-white text-black flex flex-col items-center justify-center h-screen font-BugrinoRegular">
       <img src="/loading.gif" alt="Loading" className="w-32 h-auto" />
-      <p className="text-xl">LOADING</p>
+      <p className="text-lg">LOADING</p>
     </div>
   );
 }
-
 export default Loading;
